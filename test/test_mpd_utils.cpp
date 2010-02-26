729176a590da3122daf3e5d71e9f6118290805e3
maghoff <unknown>
2010-02-26 22:06:44 +0100

    Refactored test to be data driven

--- test/test_mpd_utils.cpp
+++ test/test_mpd_utils.cpp
@@ -4,17 +4,32 @@
 #include <QByteArray>
 #include "../mpd_utils.hpp"
 
+Q_DECLARE_METATYPE(QVector<QString>);
+
 class TestMPDUtils: public QObject {
 	Q_OBJECT
 
 private slots:
+	void splitting_data();
 	void splitting();
 };
 
+void TestMPDUtils::splitting_data() {
+	QTest::addColumn<QByteArray>("coded");
+	QTest::addColumn<QVector<QString> >("decoded");
+
+	QVector<QString> tmp;
+
+	tmp.clear(); tmp << "hello";
+	QTest::newRow("basic") << QByteArray("hello\n") << tmp;
+
+	tmp.clear(); tmp << "Uncle" << "Leo!" << "Hello!";
+	QTest::newRow("multiple") << QByteArray("Uncle Leo! Hello!\n") << tmp;
+}
+
 void TestMPDUtils::splitting() {
-	QByteArray coded("hello joe\n");
-	QVector<QString> decoded;
-	decoded << "hello" << "joe";
+	QFETCH(QByteArray, coded);
+	QFETCH(QVector<QString>, decoded);
 
 	QCOMPARE(splitMessage(coded), decoded);
 }
